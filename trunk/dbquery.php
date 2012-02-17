1656848b4c709a41955acb493c4ca3fa19946cf6
karlwilcox36@gmail.com <karlwilcox36@gmail.com@ebe48706-b8e3-8995-acd7-336223b319c1>
2012-02-17 19:46:44 +0000

    Trial upload of 2.4

--- trunk/dbquery.php
+++ trunk/dbquery.php
@@ -10,17 +10,17 @@ $maxRecords = 20;
 if (isset($_GET['term'])) {
   $pass = trim(file_get_contents('../../../etc/blazondb.txt'));
   
-  $term = html_entity_decode(strip_tags ($_GET['term']));
   
   $db = @mysql_connect('localhost', 'karlwilc_blazons', $pass);
   if ( !$db ) {
     echo  "<tr><td colspan=\"4\">No database connection</td></tr>\n";
   } else {
     mysql_select_db('karlwilc_blazons');
-    $sql = "SELECT * from blazon WHERE lower(description) LIKE '%$term%';";
+    $term = mysql_real_escape_string( html_entity_decode($_GET['term']));
+    $sql = "SELECT * from blazon WHERE lower(keyname) LIKE '$term%';";
     $results = mysql_query($sql);
     if ( !$results ) {
-      echo  "<tr><td colspan=\"4\">Query failed</td></tr>\n";
+      echo  "<tr><td colspan=\"4\">Query 1 failed</td></tr>\n";
     } else {
       $count = mysql_num_rows($results);
       if ( $count > $maxRecords ) {
@@ -28,6 +28,24 @@ if (isset($_GET['term'])) {
         $count = $maxRecords;
       }
       for ( $i = 0; $i < $count; $i++ ) {
+        $record = mysql_fetch_assoc($results);
+        echo '<tr><td>'. $record['keyname'] . "</td>\n";
+        echo '<td>'. $record['description'] . "</td>\n";
+        echo '<td>'. $record['blazon'] . "</td>\n";
+        echo '<td>'. $record['source'] . "</td></tr>\n";
+      }
+    }
+    $sql = "SELECT * from blazon WHERE ((lower(description) LIKE '%$term%') AND (lower(keyname) NOT LIKE '$term%'));";
+    $results = mysql_query($sql);
+    if ( !$results ) {
+      echo  "<tr><td colspan=\"4\">Query 2 failed</td></tr>\n";
+    } else {
+      $count2 = mysql_num_rows($results);
+      if ( ($count + $count2) > $maxRecords ) {
+        echo "<tr><td colspan=\"4\">Only first $maxRecords entries shown, be more specific.</td></tr>\n";
+        $count2 = $maxRecords;
+      }
+      for ( $i = $count; $i < $count2; $i++ ) {
         $record = mysql_fetch_assoc($results);
         echo '<tr><td>'. $record['keyname'] . "</td>\n";
         echo '<td>'. $record['description'] . "</td>\n";

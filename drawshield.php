e77a8c00d7e008a1173a142a2fe120eaccd0057d
karlwilcox36 <KarlWilcox36@gmail.com>
2014-08-28 22:48:20 +0000

    Release 2.5b

--- drawshield.php
+++ drawshield.php
@@ -13,109 +13,131 @@
    limitations under the License. */
 
 //
+// Global Variables
+//
+include 'version.inc';
+
+
+//
 // Argument processing
 //
-$time_start = microtime(true);
 
-// Set default values for arguments
-$blazon = '';
-$asFile = false;
-$asText = false;
-$size = 500;
-$format = 'svg';
-$log = true;
 $p_globals = null;
 // Process arguments
-if (isset($_GET['blazon'])) $blazon = html_entity_decode(strip_tags ($_GET['blazon']));
-if (isset($_GET['format'])) $format = strip_tags ($_GET['format']);;
-if (isset($_GET['asfile'])) $asFile = true;
-if (isset($_GET['nolog'])) $log = false;
+if (isset($_GET['blazon'])) $options['blazon'] = html_entity_decode(strip_tags ($_GET['blazon']));
+if (isset($_GET['saveformat'])) $options['saveFormat'] = strip_tags ($_GET['saveformat']);;
+if (isset($_GET['asfile'])) $options['asFile'] = true;
+if (isset($_GET['palette'])) $options['palette'] = strip_tags($_GET['palette']);
+if (isset($_GET['printable'])) $options['printable'] = true;
+if (isset($_GET['highlight'])) $options['highlight'] = $_GET['highlight'] == '0' ? false : true;
 if (isset($_GET['size'])) {
   $size = strip_tags ($_GET['size']);
   if ( $size < 100 ) $size = 100;
+  $options['size'] = $size;
 }
-if ( $format != 'svg' ) $asText = true;
-if ( $format == 'svgtext' ) $format = 'svg';
 
-//
-// Global Variables
-//
-
-$tagline1 = "drawshield 2.4a";   // Advertising lines at bottom left
-$tagline2 = "karlwilcox.com/drawshield";
 
 // Quick response for empty blazon
-if ( $format == 'svg' and $blazon == '' ) {
+if ( $options['blazon'] == '' ) { // TODO "your shield here" message?
   header('Content-Type: text/xml; charset=utf-8');
-  echo '<?xml version="1.0" encoding="utf-8" ?> <svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" height="' . ($size * 1.2) . '" width="' . $size . '" viewBox="0 0 1000 1200" > <g clip-path="url(#clipPath1)"><desc>argent</desc><g><title>Shield</title><g fill="#F0F0F0"><rect x="0" y="0" width="1000" height="1200" ><title>Field</title></rect></g></g></g><defs><clipPath id="clipPath1" > <path d="M 0 0 L 0 800 A 800 400 0 0,0 500 1200 A 800 400 0 0,0 1000 800 L 1000 0 Z" /> </clipPath> </defs><text x="10" y="1160" font-size="30" >'
-   . $tagline1 . '</text><text x="10" y="1190" font-size="30" >' . $tagline2 . '</text></svg>';
-  exit;
+  $output = '<?xml version="1.0" encoding="utf-8" ?><svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" height="' .
+    ( $options['size'] * 1.2) . '" width="' .  $options['size'] . '" viewBox="0 0 1000 1200" >
+    <g clip-path="url(#clipPath1)"><desc>argent</desc><g><title>Shield</title><g fill="#F0F0F0">
+    <rect x="0" y="0" width="1000" height="1200" ><title>Field</title></rect></g></g></g>
+    <defs><clipPath id="clipPath1" > <path d="M 0 0 L 0 800 A 800 400 0 0,0 500 1200 A 800 400 0 0,0 1000 800 L 1000 0 Z" /> </clipPath></defs>
+    <text x="10" y="1160" font-size="30" >'
+   . $version['name'] . ' ' . $version['release'] . '</text><text x="10" y="1190" font-size="30" >' . $version['website'] . '</text></svg>';
+} else {
+  // Otherwise log the blazon for research... (unless told not too)
+  if ( $options['logBlazon']) error_log($options['blazon']);
+
+  // Read in the generated parsing tables
+  $charge_list = unserialize(file_get_contents('parser/data/charge_list.dat'));
+  $either_list = unserialize(file_get_contents('parser/data/either_list.dat'));
+  $ordinary_list = unserialize(file_get_contents('parser/data/ordinary_list.dat'));
+  // This is the list of words in the blazon, and the current parsing position
+  $words = array();
+  $cur_word = 0;
+  $matched_tokens = '';
+  // This is a list of charges or ordinaries awaiting a tincture
+  $pending_items = array();
+
+  // This is our XML generator and top level item for the parse tree
+  $dom = new DOMDocument('1.0');
+
+  // Read in the parsing code
+  $dir = opendir('parser');
+  while ( ($file = readdir($dir)) != false ) {
+    if ( substr($file,-4) == '.inc' ) require 'parser/' . $file;
+  }
+
+  // Set up (empty) backreferences
+  setpGlobals();
+  $parser_messages = $dom->createElement('messages');
+
+  fill_words($options['blazon']);
+
+  $node = $dom->createElement('blazon');
+  $node->setAttribute("blazonText", $options['blazon']);
+  $node->appendChild(parse());
+  //$node->appendChild(shield());
+  $bad_words = '';
+  if ( $cur_word < count($words) ) {
+    while ($cur_word < count($words))
+      $bad_words .= $words[$cur_word++] . ' ';
+    parser_message ('blazon', 'Not understood - ' . $bad_words);
+  }
+  if ( $parser_messages->hasChildNodes() ) $node->appendChild($parser_messages);
+  $dom->appendChild($node);
+
+  // Read in the drawing code
+  $dir = opendir('svg'); // All formats start out as SVG
+  while ( ($file = readdir($dir)) != false ) {
+    if ( substr($file,-4) == '.inc' ) require 'svg/' . $file;
+  }
+  $output = draw();
 }
-// Otherwise log the blazon for research... (unless told not too)
-if ( $log ) error_log($blazon);
-
-// Read in the generated parsing tables
-$charge_list = unserialize(file_get_contents('parser/data/charge_list.dat'));
-$either_list = unserialize(file_get_contents('parser/data/either_list.dat'));
-$ordinary_list = unserialize(file_get_contents('parser/data/ordinary_list.dat'));
-// This is the list of words in the blazon, and the current parsing position
-$words = array();
-$cur_word = 0;
-$matched_tokens = '';
-// This is a list of charges or ordinaries awaiting a tincture
-$pending_items = array();
-
-// This is our XML generator and top level item for the parse tree
-$dom = new DOMDocument('1.0');
-
-// Read in the parsing code
-$dir = opendir('parser');
-while ( ($file = readdir($dir)) != false ) {
-  if ( substr($file,-4) == '.inc' ) require 'parser/' . $file;
-}
-
-// Set up (empty) backreferences
-setpGlobals();
-$parser_messages = $dom->createElement('messages');
-
-fill_words($blazon);
-
-$node = $dom->createElement('blazon');
-$node->setAttribute("blazonText", $blazon);
-$node->appendChild(parse());
-//$node->appendChild(shield());
-$bad_words = '';
-if ( $cur_word < count($words) ) {
-  while ($cur_word < count($words))
-    $bad_words .= $words[$cur_word++] . ' ';
-  parser_message ('blazon', 'Not understood - ' . $bad_words);
-}
-parser_message('Disclaimer:', 'Provided for education and information only. The use of heraldic devices is restricted in many countries, independently of copyright. The granting of a Coat of Arms is solely the right of the appropriate Heraldic Authority for your nationality and country of residence.');
-if ( $parser_messages->hasChildNodes() ) $node->appendChild($parser_messages);
-$dom->appendChild($node);
-
-// Read in the drawing code
-$dir = opendir($format);
-while ( ($file = readdir($dir)) != false ) {
-  if ( substr($file,-4) == '.inc' ) require $format . '/' . $file;
-}
-
-$output = draw();
 
 // Output content header
-if ( $asFile ) {
- header("Content-type: application/force-download");
- header('Content-Disposition: inline; filename="shield.svg"');
- header("Content-Transfer-Encoding: text");
- header('Content-Disposition: attachment; filename="shield.svg"');
- header('Content-Type: image/svg+xml');
-} elseif ( $asText ) {
- header("Content-Transfer-Encoding: text");
- header('Content-Type: text/plain');
-} else {
-header('Content-Type: text/xml; charset=utf-8');
+if ( $options['asFile'] ) {
+  switch ($options['saveFormat']) {
+    case 'svg':
+     header("Content-type: application/force-download");
+     header('Content-Disposition: inline; filename="shield.svg"');
+     header("Content-Transfer-Encoding: text");
+     header('Content-Disposition: attachment; filename="shield.svg"');
+     header('Content-Type: image/svg+xml');
+     echo $output;
+     break;
+    case 'jpg':
+      $im = new Imagick();
+      $im->readimageblob($output);
+      $im->setimageformat('jpeg');
+      $im->setimagecompressionquality(90);
+      // $im->scaleimage(1000,1200);
+      header("Content-type: application/force-download");
+      header('Content-Disposition: inline; filename="shield.jpg"');
+      header("Content-Transfer-Encoding: binary");
+      header('Content-Disposition: attachment; filename="shield.jpg"');
+      header('Content-Type: image/jpg');
+      echo $im->getimageblob();
+      break;
+    case 'png':
+    default:
+     $im = new Imagick();
+     $im->readimageblob($output);
+     $im->setimageformat('png');
+     // $im->scaleimage(1000,1200);
+     header("Content-type: application/force-download");
+     header('Content-Disposition: inline; filename="shield.png"');
+     header("Content-Transfer-Encoding: binary");
+     header('Content-Disposition: attachment; filename="shield.png"');
+     header('Content-Type: image/png');
+     echo $im->getimageblob();
+     break;
+   }
+} else { // just send it back as SVG
+  header('Content-Type: text/xml; charset=utf-8');
+  echo $output;
 }
 
-echo $output;
-
-?>
--- tools/build_data.php
+++ tools/build_data.php
@@ -11,12 +11,88 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License. */
+if (! ( gethostname() == 'Home-Laptop' or isset($_GET['execute']) ) ) {
+	echo "You can't play here\n";
+	exit;
+}
 
-set_include_path('C:\Users\Public\www\include\shield');
+set_include_path('..');
+$target_dir = '../parser/data/';
    
 function draw_message($category,$message) { echo $message; }
 function get_mod ( $node, $name ) { return null; }
-function rgb( $colour ) { return null; }
+function rgb( $colour ) { return $colour; }
+
+function get_main_name($patterns) {
+  $pattern = $patterns[0];
+  $words = explode(' ',$pattern);
+  $retval = '';
+  foreach ( $words as $word ) {
+    if ( $word{0} == '?' ) continue;
+    $word = preg_replace('/s\?/','',$word);
+    $word = preg_replace('/\?/','',$word);
+    $word = preg_replace('/\|.*?\)/',')',$word);
+    $word = preg_replace('/\((.*?)\)/','\1',$word);
+    $retval .= $word . ' ';
+  }
+  $retval = trim($retval);
+  return $retval;
+}
+
+function get_alt_names($patterns, $main = '') {
+  $names = array();
+  foreach ($patterns as $pattern) {
+    $cur_names = array ( '' );
+    $words = explode ( ' ', $pattern );
+    foreach ( $words as $word ) {
+      $word = preg_replace('/s\?/','',$word);
+      $word = preg_replace('/\?/','',$word);
+      $cur_words = array ( '' );
+      $in_brackets = false;
+      $temp = '';
+      for ( $i = 0; $i < strlen($word); $i++ ) {
+        if ( $word{$i} == '(' ) {
+          $in_brackets = true;
+          $i++;
+        }
+        elseif ( $word{$i} == ')' ) {
+          $alts = explode('|',$temp);
+          $orig_num_words = count($cur_words);
+          for ( $k = 1; $k < count($alts); $k++ ) {
+            for ( $j = 0; $j < $orig_num_words; $j++ )
+              $cur_words[] .= $cur_words[0] .  $alts[$k];
+          }
+          for ( $j = 0; $j < $orig_num_words; $j++ )
+            $cur_words[$j] .= $alts[0];
+          $temp = '';
+          $in_brackets = false;
+          $i++;
+        }
+        if ( $in_brackets )
+          $temp .= $word{$i};
+        elseif ( $i < strlen($word))
+          for ( $j = 0; $j < count($cur_words); $j++ )
+            $cur_words[$j] .= $word{$i};
+      }
+      $orig_num_names = count($cur_names);
+      for ( $i = 1; $i < count($cur_words); $i++ )
+        for ( $j = 0; $j < $orig_num_names; $j++ )
+          $cur_names[] = $cur_names[0] . ' ' . $cur_words[$i];
+      for ( $i = 0; $i < $orig_num_names; $i++ )
+        $cur_names[$i] .= ' ' . $cur_words[0];
+    }
+    $names = array_merge($names, $cur_names);
+  }
+  for ( $i = 0; $i < count($names); $i++ ) {
+    $names[$i] = trim($names[$i]);
+    if ( $names[$i] == $main ) {
+      $names[$i] = '';
+      continue;
+    }
+  }
+  return $names;
+}
+
 
 function include_charge () {
   $node = null;
@@ -37,6 +113,7 @@ $node = null;
   $charges = array();
   $ordinaries = array();
   $either = array();
+  $chargeList = '"Names","Type","Features","Modifiers","Ignored","Proper","Flags","SVG","Filename"' . "\n";
   // Go through charges
   $dir = opendir('../charges');
   echo "Building charges...\n";
@@ -44,23 +121,101 @@ $node = null;
     if ( $subdir{0} != '.' and is_dir('../charges/' . $subdir) ) {
       $ddir = opendir('../charges/' . $subdir );
       while ( ($file = readdir($ddir)) != false ) {
+        // Set up charge list items
+        $features = '';
+        $mods = '';
+        $ignored = '';
+        $proper = '';
+        $flags = '';
+        $source = 'none';
+        $name = '';
+        // Go through each item
         if ( ($file{0} != '_') and substr($file,-4) == '.inc' ) {
         	$type = $subdir;
         	$subtype = substr($file,0,strlen($file)-4);
         	//echo "\r$subtype";
-            $charge = include_charge ( '../charges/' . $subdir . '/' . $file );
-            if ( array_key_exists('file',$charge) and ! file_exists('../charges/' . $type . '/' . $charge['file']))
-              echo "Warning: file " .  $charge['file'] . " not found\n"; 
-            $def_col = array();
-            // Add this to the either list, for disambiguation?
-            if ( array_key_exists ( 'either', $charge ) ) {
-              foreach ( $charge['patterns'] as $pattern )
-                $either[] = array ( $subtype, $pattern, $type, 'charge');
-            } else {
-              foreach ( $charge['patterns'] as $pattern ) {
-                $charges[] = array ( $subtype, $pattern, $type, 'charge');
+          $charge = include_charge ( '../charges/' . $subdir . '/' . $file );
+          if ( array_key_exists('file',$charge) and ! file_exists('../charges/' . $type . '/' . $charge['file']))
+            echo "Warning: file " .  $charge['file'] . " not found\n";
+          // Gather data for charge list
+          foreach ( $charge as $item => $data ) {
+            switch ($item) {
+            case 'modifiers':
+              $featArray = array();
+              $modArray = array();
+              $ignoreArray = array();
+              foreach ($data as $modifier ) {
+                switch ( $modifier[2]) {
+                  case 'feature':
+                    $featArray[] = $modifier[1];
+                    break;
+                  case 'warn':
+                  case 'ignore':
+                    $ignoreArray[] = $modifier[1];
+                    break;
+                  case 'mod':
+                    $modArray[] = $modifier[1];
+                    break;
+                 }
               }
+              if ( count ( $featArray)) $features = implode (get_alt_names($featArray), ' / ');
+              if ( count ( $modArray)) $mods = implode (get_alt_names($modArray), ' / ');
+              if ( count ( $ignoreArray)) $ignored = implode (get_alt_names($ignoreArray), ' / ');
+              break;
+              case 'features':
+                foreach ($data as $feature => $details) {
+                  if ( array_key_exists('proper',$details))
+                    $proper .= '(' . $feature . ' ' . $details['proper'] . ') ';
+                }
+                break;
+              case 'proper':
+              case 'default':
+                if ( is_array($data))
+                  $proper .= implode(',', $data) . ' ';
+                else
+                  $proper .= $data . ' ';
+                break;
+              case 'file':
+                $source = str_replace('inkscape/','',$data);
+                break;
+              case 'body':
+                $source = 'built-in';
+                break;
+              case 'patterns':
+                // $name = implode($data,' ');
+                $name = implode(get_alt_names($data),' / ');
+                break;
+              case 'doc':
+                // To be deleted, never used
+                break;
+              case 'either':
+              case 'flags':
+              // data for use in parser so don't care, do nothing
+              break;
+              case 'licence':
+              case 'height':
+              case 'width':
+                echo "Warning, obsolete field in $subtype\n";
+                // obsolete soon I hope!
+                break;
+              default:
+                if ( is_array($data))
+                  echo "$item is array in $subtype\n";
+                else
+                  $flags .= "$item=$data ";
+                break;
             }
+          }
+          $chargeList .= "\"$name\",\"$type\",\"$features\",\"$mods\",\"$ignored\",\"$proper\",\"$flags\",\"$source\",\"$file\"\n";
+          // Add this to the either list, for disambiguation?
+          if ( array_key_exists ( 'either', $charge ) ) {
+            foreach ( $charge['patterns'] as $pattern )
+              $either[] = array ( $subtype, $pattern, $type, 'charge');
+          } else {
+            foreach ( $charge['patterns'] as $pattern ) {
+              $charges[] = array ( $subtype, $pattern, $type, 'charge');
+            }
+          }
         }
       }
     }
@@ -106,18 +261,19 @@ $node = null;
   echo "\ncompleted\n";
   // Write out the data files
   echo "Writing files...";
-  $fp = fopen('../parser/data/charge_list.dat','w');
+  $fp = fopen($target_dir . 'charge_list.dat','w');
   fwrite ( $fp, serialize($charges) );
   fclose ( $fp );
   unset ( $charges );
-  $fp = fopen('../parser/data/ordinary_list.dat','w');
+  $fp = fopen($target_dir . 'ordinary_list.dat','w');
   fwrite ( $fp, serialize($ordinaries) );
   fclose($fp);
   unset ( $ordinaries );
-  $fp = fopen('../parser/data/either_list.dat','w');
+  $fp = fopen($target_dir . 'either_list.dat','w');
   fwrite ( $fp, serialize($either) );
   fclose($fp);
   unset ( $either );
+  file_put_contents('charges.csv', $chargeList);
   echo "completed\n";
 
  /* Fudge if web server can't write to folder, open in web browser
@@ -129,4 +285,3 @@ $node = null;
  echo serialize($modifiers) . "\n";
  echo "either_list\n";
  echo serialize($either) . "\n";   */
-?>
